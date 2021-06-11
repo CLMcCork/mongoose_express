09439b5f7d8f12194d6b0322bb8bdcd333763891
CrystalMcCorkle <crystallmccorkle@gmail.com>
2021-06-11 14:33:17 -0500

    added logic and routes to create a product

--- index.js
+++ index.js
@@ -19,15 +19,29 @@ mongoose.connect('mongodb://localhost:27017/farmStand', {useNewUrlParser: true, 
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}))
 
-//Index (list of current products in database)--takes awhile so use an async function
+//Index (list of all current products in database)--takes awhile so use an async function
 app.get('/products', async (req, res) => {
     const products = await Product.find({}) //await this mongoose operation //sends back all products 
     //console.log(products) to see if it's working 
     res.render('products/index', { products }) //renders the index.ejs file //then passes through all of the products that are found
 })
 
-//view details about a single product 
+//brings up the form to create a new product
+//submits the form to a post route
+app.get('/products/new', (req, res) => {
+    res.render('products/new')
+})
+
+//submits the form, creates new product in db, and redirects 
+app.post('/products', async (req, res) => {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.redirect(`/products/${newProduct._id}`)
+})
+
+//view details about a single product by id 
 app.get('/products/:id', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id)
